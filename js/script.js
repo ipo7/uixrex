67c7a6b55176fe5d71fa6385c453864498085371
ipo7 <61191405+ipo7@users.noreply.github.com>
2020-04-25 13:10:51 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$(function() {
+
+  $('.burger').on('click touchend', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    $('.header__menu').slideToggle(300);
+  });
+
+  // $('.price-item').on('mouseenter touchend', function(e) {
+  //
+  //   if ($(this).hasClass('price-item_active')) {
+  //     $(this).removeClass('price-item_active')
+  //   } else {
+  //     $(this).addClass('price-item_active');
+  //   }
+  // });
+
+  $('.screenshots__content').slick({
+    //adaptiveHeight: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    //centerMode: true,
+    focusOnSelect: true,
+
+    speed: 800,
+    easing: 'ease',
+    waitForAnimate: false,
+
+    //arrows: false,
+
+    nextArrow: '.screenshots__next-button',
+    prevArrow: '.screenshots__previous-button',
+
+    responsive: [{
+        breakpoint: 1200, // максимальная ширина экрана
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          //arrows: true,
+        }
+      },
+      {
+        breakpoint: 767, // максимальная ширина экрана
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ],
+
+  });
+
+});
